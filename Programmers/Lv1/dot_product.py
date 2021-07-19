6543b46ae90b443ce5fbfe0413fbb02051b6d3c6
sunwoo <cocok14@aistudio.co.kr>
2021-07-19 09:12:56 +0900

    2021-07-19 : 프로그래머스 Lv1. 내적

---
+++ Programmers/Lv1/dot_product.py
@@ -0,0 +1,10 @@
+# 내적
+def solution(a, b):
+    answer = 0
+    for a_num, b_num in zip(a, b):
+        answer += a_num * b_num
+
+    # another solution
+    # answer = sum(map(lambda i: a[i] * b[i], range(len(a))))
+
+    return answer
