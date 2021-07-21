44bb5ddd11654f922696f1a47edd3ba08e02897f
sunwoo <cocok14@aistudio.co.kr>
2021-07-21 09:45:23 +0900

    2021-07-21 : 프로그래머스 Lv1. 완주하지 못한 선수

---
+++ Programmers/Lv1/runners.py
@@ -0,0 +1,25 @@
+# 완주하지 못한 선수
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+        return participant[-1]
+
+    # another solution - 1. use collections.Counter
+    # import collections
+    # answer = collections.Counter(participant) - collections.Counter(completion)
+    # return list(answer)[0]
+
+    # another solution - 2. use hash
+    # temp = 0
+    # dic = {}
+    # for part in participant:
+    #     dic[hash(part)] = part
+    #     temp += int(hash(part))
+    # for com in completion:
+    #     temp -= hash(com)
+    #
+    # return dic[temp]
